f4f86a416284ab33e0a7bf64609b25de91feec74
x-mitsui <x_mitsui@163.com>
2025-10-21 17:06:54 +0800

    tip: state's setter 结合 emiiter很好用

--- assets/scripts/ui/BombUI.ts
+++ assets/scripts/ui/BombUI.ts
@@ -2,7 +2,7 @@ import { _decorator } from "cc";
 import { eventManager } from "../utils/EventManager";
 import { PLAYER_CHANGE_BOMB_COUNT } from "../utils/Event";
 import { CustomUI } from "./CustomUI";
-const { ccclass, property } = _decorator;
+const { ccclass } = _decorator;
 
 @ccclass("BombUI")
 export class BombUI extends CustomUI {
